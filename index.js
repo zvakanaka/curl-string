bf147b346fbb432613fef008d135f4274a514766
zvakanaka <adam.quinton@familysearch.org>
2019-10-07 14:27:35 -0600

    handle content-type of x-www-form-urlencoded

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const cj = require('color-json');
+const hasOwnProperty = (object, property) => Object.prototype.hasOwnProperty.call(object, property);
+
+module.exports = curlString;
 
 /**
  * Builds a curl command and returns the string.
- * @param  {String} url     Endpoint
- * @param  {Object} options Object with headers, etc. (fetch format)
- * @return {String}         Curl command
+ * @param  {String} url               Endpoint
+ * @param  {Object} options           Object with headers, etc. (fetch format)
+ * @param  {Object} [curlStringOptions={colorJson:true,jsonIndentWidth:2}] Formatting options
+ * @return {String}                   cURL command
  */
 function curlString(
   url,
@@ -33,25 +37,54 @@ function curlString(
   }
 
   if (hasBody) {
-    let parsedData;
-    try {
-      parsedData = JSON.parse(options.body);
-    } catch (e) {
-      parsedData = options.body;
-    }
-
-    const colorJson = curlStringOptions && curlStringOptions.colorJson;
-    const jsonIndentWidth =
-      (curlStringOptions && curlStringOptions.jsonIndentWidth) || 2;
-
-    curl += `\n--data '${
-      colorJson
-        ? cj(parsedData)
-        : JSON.stringify(parsedData, null, jsonIndentWidth)
-    }'`;
+    curl += `\n--data '${bodyToDataString(options, curlStringOptions)}'`
   }
 
   return curl;
 }
 
-module.exports = curlString;
+function getHeader(options, headerKeyName) {
+  return options.headers[headerKeyName];
+}
+
+function hasHeader(options, headerKeyName) {
+  return hasOwnProperty(options, 'headers') &&
+    Object.keys(options.headers)
+      .find(header => headerKeyName === header.toLowerCase());
+}
+
+/**
+ * Constructs a body string for use inside --data
+ * @param  {Object} options           Object with headers, etc. (fetch format)
+ * @param  {Object} curlStringOptions Formatting options
+ * @return {String}                   cURL command data string
+ */
+function bodyToDataString(options, curlStringOptions) {
+  let parsedData;
+  try {
+    parsedData = JSON.parse(options.body);
+  } catch (e) {
+    // fall back to original body if it could not be parsed as JSON
+    parsedData = options.body;
+  }
+
+  // return an ampersand delimited string
+  if (hasHeader(options, 'content-type') &&
+      getHeader(options, 'content-type').toLowerCase() === 'application/x-www-form-urlencoded') {
+    if (typeof parsedData === 'string') {
+      return parsedData;
+    } else {
+      return Object.entries(parsedData)
+        .map(([key, val]) => `${key}=${val}`)
+        .join('&');
+    }
+  } else {
+    const colorJson = curlStringOptions && curlStringOptions.colorJson;
+    const jsonIndentWidth =
+      (curlStringOptions && curlStringOptions.jsonIndentWidth) || 2;
+
+    return colorJson
+      ? cj(parsedData)
+      : JSON.stringify(parsedData, null, jsonIndentWidth);
+  }
+}
